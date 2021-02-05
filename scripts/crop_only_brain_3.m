1bb36a3b444f401013da2250af1b363db7700a75
Pam Garcia <np.pam.garcia@comunidad.unam.mx>
2021-02-05 14:46:34 -0600

    Update crop_only_brain_3.m

--- scripts/crop_only_brain_3.m
+++ scripts/crop_only_brain_3.m
@@ -21,7 +21,7 @@ function image_crop=crop_only_brain_3(path_job,nii,outname)
         Z_dim=round(62/voxel_size_z); %55 mm max size monkey brain
 
         center_image=[fov_x,fov_y,fov_z];
-        
+
         axis_x_start=round((X_dim/2)+center_image(1,1));
         axis_x_end=round(center_image(1,1)-(X_dim/2));
 
@@ -30,10 +30,9 @@ function image_crop=crop_only_brain_3(path_job,nii,outname)
 
         axis_z_start=round((Z_dim/2)+center_image(1,3));
         axis_z_end=round(center_image(1,3)-(Z_dim/2));
-        
+
 
         if  axis_x_end <= 0 || axis_x_start >= fov_size (1,1)
-
         axis_y=[axis_y_end,axis_y_start];
         axis_z=[axis_z_end,axis_z_start];
 
@@ -63,11 +62,9 @@ function image_crop=crop_only_brain_3(path_job,nii,outname)
         image_crop=[image_crop,' -axis 2 ',axis_z];
         s=''''
         eval([ 'dlmwrite(' s crop_out s ',image_crop,' s 'delimiter' s ',' s '' '' s ')' ])
-
         end
 
         if  axis_z_end <= 0 || axis_z_start >= fov_size (1,3)
-
         axis_x=[axis_x_end,axis_x_start];
         axis_y=[axis_y_end,axis_y_start];
 
@@ -78,17 +75,33 @@ function image_crop=crop_only_brain_3(path_job,nii,outname)
         image_crop=[image_crop,' -axis 1 ',axis_y];
         s=''''
         eval([ 'dlmwrite(' s crop_out s ',image_crop,' s 'delimiter' s ',' s '' '' s ')' ])
-
         end
 
-        if axis_x_end <= 0 || axis_x_start >= fov_size (1,1) 
-            if axis_z_end <= 0 || axis_z_start >= fov_size (1,3)
-        axis_y=[axis_y_end,axis_y_start];
-        axis_y=num2str(axis_y);
+        if axis_x_end <= 0 || axis_x_start >= fov_size (1,1)
+            if axis_y_end > 0
+                 if axis_z_end <= 0 || axis_z_start >= fov_size (1,3)
+                    axis_y=[axis_y_end,axis_y_start];
+                    axis_y=num2str(axis_y);
+                    image_crop=['-axis 1 ', axis_y];
+                    s=''''
+                    eval([ 'dlmwrite(' s crop_out s ',image_crop,' s 'delimiter' s ',' s '' '' s ')' ])
+                 end
+            end
+        end
 
-        image_crop=['-axis 1 ', axis_y];
-        s=''''
-        eval([ 'dlmwrite(' s crop_out s ',image_crop,' s 'delimiter' s ',' s '' '' s ')' ])
-            end 
+        if axis_x_end <= 0 && axis_y_end <= 0 && axis_z_end <= 0
+                axis_x=[0,fov_size(1,1)];
+                axis_y=[0,fov_size(1,2)];
+                axis_z=[0,fov_size(1,3)];
+
+                axis_x=num2str(axis_x);
+                axis_y=num2str(axis_y);
+                axis_z=num2str(axis_z);
+
+                image_crop=['-axis 0 ', axis_x]
+                image_crop=[image_crop,' -axis 1 ',axis_y];
+                image_crop=[image_crop,' -axis 2 ',axis_z];
+                s=''''
+                eval([ 'dlmwrite(' s crop_out s ',image_crop,' s 'delimiter' s ',' s '' '' s ')' ])
         end
 end
