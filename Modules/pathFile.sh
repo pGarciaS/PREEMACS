f5fb2f6cb7ef12fc5eb31186375235be31ba1322
Luis Concha <lconcha@gmail.com>
2024-05-13 15:05:05 -0600

    check for pytorch

--- Modules/pathFile.sh
+++ Modules/pathFile.sh
@@ -11,7 +11,7 @@
 #Do not change after this
 
 #FSL
-echo "Checking FSL"
+echo "Checking FSL ..."
 if [ ! -d "$FSLDIR" ]; then
 		echo "FSL not installed"; 
 		exit $ERRCODE; 
@@ -19,7 +19,7 @@ fi
 echo "  FSL is found at ${FSLDIR}" 
 
 #FREESURFER
-echo "Checking Freesurfer"
+echo "Checking Freesurfer ..."
 	if [ ! -d "$FREESURFER_HOME" ]; then
 		echo "Freesurfer not installed"; 
 		exit $ERRCODE; 
@@ -27,7 +27,7 @@ echo "Checking Freesurfer"
 echo "  FREESURFER is found at ${FREESURFER_HOME}" 
 
 #MATLAB
-echo "Checking MATLAB"
+echo "Checking MATLAB ..."
 if [ -z $(which matlab) ]; then
 		echo "matlab not installed"; 
 		exit $ERRCODE; 
@@ -37,7 +37,7 @@ export matlab_path=$(dirname $matlab_bin)
 echo "  MATLAB is found at ${matlab_path}" 
 
 #MRTRIX
-echo "Checking MRTRIX"
+echo "Checking MRTRIX ..."
 if [ -z $(which mrcalc) ]; then
 		echo "MRTRIX not installed"; 
 		exit $ERRCODE; 
@@ -47,7 +47,7 @@ MRTRIX_DIR=$(dirname $mrcalc_bin)
 echo "  MRTRIX is found at ${MRTRIX_DIR}" 
 
 #ANTS
-echo "Checking ANTS"
+echo "Checking ANTS ..."
 if [ ! -d $ANTSPATH ]; then
 		echo "ANTS not installed"; 
 		exit $ERRCODE; 
@@ -55,4 +55,13 @@ fi
 export ants_path=$ANTSPATH
 echo "  ANTs is found at ${ants_path}" 
 
+
+# Pytorch
+echo "Checking pytorch ..."
+python -c "import torch"
+if [ $? -ne 0 ]; then
+		echo "pytorch not installed"; 
+		exit $ERRCODE; 
+fi
+echo "  Pytorch module exists." 
 
